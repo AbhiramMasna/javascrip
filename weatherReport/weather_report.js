70c9cee16778d787cbf1841dbe6d7f98c31c1522
AbhiramMasna <masnaabhiram99@gmail.com>
2024-02-11 05:57:12 -0500

    fetch method usage

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,26 @@
+function showWeatherDetails(event) {
+    event.preventDefault();
+    
+    const city = document.getElementById('city').value;
+    const apiKey = 'ee88243fd9b4a22852b1a3860f15a9ff'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                <p>Temperature: ${data.main.temp} &#8451;</p>
+                                <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather data:', error);
+    });
+}
+
+document.getElementById('weatherForm').addEventListener('submit', showWeatherDetails);
